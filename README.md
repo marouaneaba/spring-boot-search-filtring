495012f03cb5120d7f10b5627f4121df58b455d8
marouaneabk <marouane.abakarim@decathlon.com>
2023-02-03 14:37:49 +0100

    feat(search): filtring data with specification

---
+++ README.md
@@ -0,0 +1,5 @@
+# spring-boot-search-filtring
+
+L'objectif de ce repositories, c'est implementer le filtring avec:
+- JpaSpecificationExecutor
+- JOOQs
---
+++ src/main/java/com/marouane/data/filtringspecification/FilteringDataSpecification.java
@@ -0,0 +1,11 @@
+package com.marouane.data.filtringspecification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilteringDataSpecification {
+    public static void main(String... args) {
+        SpringApplication.run(FilteringDataSpecification.class, args);
+    }
+}
